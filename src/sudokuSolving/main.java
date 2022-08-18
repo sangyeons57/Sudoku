07e1f879c1212b13d21b7b6aaf9a2323cc6e6876
sangyeons57 <sangyeons57@gmail.com>
2022-08-18 21:13:47 +0900

    test commit create parse_grid

--- src/sudokuSolving/main.java
+++ src/sudokuSolving/main.java
@@ -1,9 +1,14 @@
 package sudokuSolving;
 
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class main {
 	
@@ -12,40 +17,68 @@ public class main {
 		String digits = "123456789";
 		String rows = "ABCDEFGHI";
 		String cols = digits;
-		List<String> squares = cross(digits , rows);
+		List<String> squares = cross(rows , cols);
 		List<String> colsList = List.of("ABC","DEF","GHI");
 		List<String> rowsList = List.of("123","456","789");
 		
 		List<List<String>> unitlist = new ArrayList<List<String>>();
 		
-		Map<String, List<String>> units = new HashMap<>();
-		Map<String, List<String>> peers = new HashMap<>();
+		Map<String, List<String>> units = new HashMap<String, List<String>>();
+		Map<String, Set<String>> peers = new HashMap<String, Set<String>>();
 		
+		//unit list make
+		//cols line unit
 		for (char c : cols.toCharArray())
 		{
 			unitlist.add(cross(rows, "" + c));
 		}
+		//rows line unit
 		for (char r : rows.toCharArray())
 		{
 			unitlist.add(cross(rows, "" + r));
 		}
+		//box unit
 		for ( int i =0; i < 3; i++)
 		{
 			unitlist.add( cross(colsList.get(i), rowsList.get(i) ) );
 		}
-		System.out.println(unitlist);
 		
+		
+		//make units : 3 kind unit -> dict (main : units)
 		for ( String s : squares)
 		{
 			for (List<String> u : unitlist)
 			{
 				if (u.contains(s))
 				{
-					
+					units.put(s, u);
 				}
 			}
 		}
+		
+		//make peers: be able to peers about main square
+		for (String s : squares )
+		{
+			Set<String> set = new HashSet<String>();
+			set.addAll( units.get(s));
+			set.remove(s);
+			peers.put(s, set);
+		}
+		System.out.println(peers);
+
 	}
+
+	public Map<String,List<String>> parse_grid(Map<String,List<String>> grid, List<String> squares, String digits) 
+	{
+		Map<String, String> values = new HashMap<String, List<String>>();
+		for(String s : squares)
+		{
+			values.put(s, digits);
+		}
+		
+
+	}
+	
 	
 	
 	static public List<String> cross(String _a, String _b) 
